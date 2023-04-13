1f15cb083256bc81f014ea2aff9c8425ac635244
Ruruthia <maryswyrz@gmail.com>
2023-04-13 15:14:08 +0200

    Add simple REST API

---
+++ src/run_server.py
@@ -0,0 +1,24 @@
+import numpy as np
+from flask import Flask, request, jsonify
+
+from src.utils import load_models
+
+app = Flask(__name__)
+models = load_models()
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    point = data['input']
+    model_type = data['model_type']
+
+    model = models.get(model_type, None)
+    if model is None:
+        return jsonify({'error': 'model_type should be one of: simple_heuristic, knn, random_forest, neural_network'})
+    prediction = model.predict(np.array(point).reshape(1, -1)).item()
+    return jsonify({'prediction': prediction})
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,18 @@
+import pickle
+from typing import Union, Dict
+
 import numpy as np
 import pandas as pd
 import seaborn as sn
 import tensorflow as tf
+import yaml
 from matplotlib import pyplot as plt
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
+
+from src.models.neural_net import NeuralNet
+from src.models.scikit_models import ScikitModel
+from src.models.simple_heuristic import SimpleHeuristic
 
 MAX_EPOCHS = 15
 
@@ -20,9 +28,9 @@ def load_dataset() -> (pd.DataFrame, pd.DataFrame):
 def split_dataset(X: pd.DataFrame, y: pd.DataFrame, test_size: float = 0.2) \
         -> (pd.DataFrame, pd.DataFrame, pd.Series, pd.Series):
     train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=test_size)
-    
+
     # we want the labels to start from 0
-    return train_X, test_X, np.ravel(train_y - 1) , np.ravel(test_y - 1)
+    return train_X, test_X, np.ravel(train_y - 1), np.ravel(test_y - 1)
 
 
 def calculate_accuracy(y: pd.Series, predicted_y: pd.Series) -> float:
@@ -44,3 +52,17 @@ def plot_confusion_matrix(y: pd.Series, predicted_y: pd.Series) -> None:
     cm = metrics.confusion_matrix(y_true=y, y_pred=predicted_y)
     sn.heatmap(cm, annot=True, fmt='g')
     plt.show()
+
+
+def load_models() -> Dict[str, Union[SimpleHeuristic, NeuralNet, ScikitModel]]:
+    models = {}
+    for model in ["knn", "random_forest"]:
+        with open(f"models/{model}.pkl", "rb") as f:
+            models[model] = pickle.load(f)
+    with open("/home/maria/Documents/OpenX/configs/neural_net.yaml") as f:
+        config = yaml.safe_load(f)
+    models["simple_heuristic"] = SimpleHeuristic()
+    nn = NeuralNet(**config, max_epochs=15)
+    nn.load_from_checkpoint("models/neural_net.ckpt")
+    models["neural_network"] = nn
+    return models
