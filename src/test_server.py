f22db605a2e241af6846efc5692a7809363233b2
Maria Wyrzykowska <maryswyrz@gmail.com>
2023-04-13 15:15:20 +0200

    Add simple REST API (#7)

---
+++ src/test_server.py
@@ -0,0 +1,14 @@
+import requests
+
+from src.utils import load_dataset
+
+X, y = load_dataset()
+headers = {
+    'Content-Type': 'application/json',
+}
+
+data = {"input": list(X.iloc[0]), "model_type": "random_forest"}
+
+response = requests.post('http://localhost:5000/predict', headers=headers, json=data)
+prediction = response.json()['prediction']
+print(f'Predicted class: {prediction}')
